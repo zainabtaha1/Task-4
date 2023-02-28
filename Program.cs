8e5a9f3e4a0114f8b42818e8c89c4cb1b90ba504
zainabtaha1 <121588138+zainabtaha1@users.noreply.github.com>
2023-02-28 03:54:42 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Problem 4
+int x = 7, y = 4;
+// عرف متغيرين X,Y وحطيت لهم قيمة مبدئية
+x = 6;
+// غيرت قيمة المتغير X
+int result;
+// عرف متغير اسمه result
+result = x - y;
+Console.WriteLine("The first number is :{0}", x);
+// xوحط قيمهThe first number is اطبع 
+Console.WriteLine("The second number is :{0} ", y );
+// yوحط قيمهThe second number is اطبع
+Console.WriteLine("The result is : " + result);
+// resultوحط قيمة The result is اطبع 
+Console.WriteLine("The substraction of {0} and {1} = {2}", x , y , result );
+/* {0} مكان x وحط  The substraction ofاطبع 
+ *{1} مكان y وحط 
+ *{2} مكان result وحط 
+ */
